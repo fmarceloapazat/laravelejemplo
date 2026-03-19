dc7c110315756d9c8824dbb8dbd74637766c47fe
Ernesto Enviamas <ernestoenviamas@Ernestos-MacBook-Air.local>
2026-03-19 16:29:01 -0500

    asdasd

--- public/index.php
+++ public/index.php
@@ -11,6 +11,7 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
     require $maintenance;
 }
 
+$dni = "747555556";
 
 // Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
